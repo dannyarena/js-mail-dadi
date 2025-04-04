9a86d0260a8368bda361383d76c923038d42165a
dannyarena <arenadanny71@gmail.com>
2025-04-04 17:05:29 +0200

    Add email validation feature

---
+++ script.js
@@ -0,0 +1,16 @@
+// 1. Creo una lista di email invitate alla festa
+// 2. Prendo l'email inserita dall'utente cliccando il bottone
+// 3. Uso un ciclo for per confrontare ogni email della lista con quella inserita
+// 4. Se trovo una corrispondenza, l'utente può entrare
+// 5. Altrimenti, l'accesso è negato
+// 6. Mostro il risultato nel paragrafo sotto al bottone
+
+
+const listaEmail = [
+    "mario@example.com",
+    "luigi@example.com",
+    "peach@example.com",
+    "danny@example.com",
+    "toad@example.com"
+  ];
+  
